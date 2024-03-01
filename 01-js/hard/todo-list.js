3f0f77181f843a367c4efad893dcf118943dc6ac
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-01 22:42:00 +0530

    Todo List

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -10,8 +10,47 @@
   Once you've implemented the logic, test your code by running
 */
 
+
 class Todo {
 
+  todoCon
+
+  constructor() {
+    this.todoCon = []
+  }
+
+  add(todo) {
+    this.todoCon.push(todo)
+  }
+
+  remove(indexOfTodo = 1) {
+    if (indexOfTodo < 0) { return false }
+
+    for (let index = 0; index < this.todoCon.length; index++) {
+      if (index === indexOfTodo) {
+        this.todoCon[index] = '-1'
+      }
+    }
+
+    console.log(this.todoCon);
+
+  }
+
+  getAll() {
+    for (let index = 0; index < this.todoCon.length; index++) {
+      (this.todoCon[index] != -1) ?
+        console.log(this.todoCon[index]) : null
+    }
+  }
 }
+const obj = new Todo();
+
+obj.add('A')
+obj.add('B')
+obj.add('C')
+obj.add('D')
+obj.getAll()
+obj.remove(0)
+obj.getAll()
 
 module.exports = Todo;
