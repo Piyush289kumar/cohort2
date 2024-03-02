5ddc1bd55abd03237f073eafcd688be73741df7e
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-02 17:58:43 +0530

    Demo

--- http-server-1/index.js
+++ http-server-1/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.post('/app', (req, res) => {
-    console.log(req);
+app.get('/app', (req, res) => {
+    
     res.send('2 + 2 = 4')
 })
 
