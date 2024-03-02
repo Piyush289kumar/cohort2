180a13bb24568c3fb0915ad6b35011d7128f35a6
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-02 19:55:16 +0530

    body-parser middleware

--- http-server-1/index.js
+++ http-server-1/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const bodyParse = require("body-parser")
 const app = express()
 const port = 3000
 
-app.get('/app', (req, res) => {
-    
+
+app.use(bodyParse.json());
+
+app.post('/app', (req, res) => { 
+    console.log(req.body);   
     res.send('2 + 2 = 4')
 })
 
