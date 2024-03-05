231e979074a8b11cf8303f31bfda6d229d902726
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-05 10:58:32 +0530

    Postman

--- week-02/express.class/script.js
+++ week-02/express.class/script.js
@@ -1,34 +1,39 @@
 const express = require('express')
 
 const app = express();
+app.use(express.json())
 
-const user = {
+const user = [{
     name: 'UserName',
     kidney: {
         count: 2,
         healthKidneyCount: 1,
     }
-}
+}]
 
 
 app.get('/', (req, res) => {
 
-    const objName = user.name
-    const kidneyCount = user.kidney.count
-    const healthKidneyCount = user.kidney.healthKidneyCount
+    const objName = user[0].name
+    const kidneyCount = user[0].kidney.count
+    const healthKidneyCount = user[0].kidney.healthKidneyCount
 
     res.send(`Your Name is ${objName} || Kidney Count : ${kidneyCount} || Healthy Kidneys : ${healthKidneyCount}`)
 })
 
 
-app.delete('/', (req, res) => {
-    const objName = user.name
-    const kidneyCount = user.kidney.count
-    const healthKidneyCount = user.kidney.healthKidneyCount
+app.post('/', (req, res) => {
+    const isHealthy = req.body.isHealthy;
 
-    const deleteCount = req.query.d;
+    user[0].kidney.count += isHealthy
 
-    res.send(`Your Name is ${objName} || Kidney Count : ${kidneyCount - deleteCount} || Healthy Kidneys : ${healthKidneyCount}`)
+    res.json({
+        msg: "done"
+    })
+
+
 })
+
+
 
 app.listen(3000)
